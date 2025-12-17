2b55cf8c9a947ea45bcf54a5f030f78123dbb04a
blackezrah <black.ezra.h@gmail.com>
2025-12-17 14:12:48 -0500

    Reposition hero background to move shooting star left

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -31,7 +31,10 @@ export function HeroSection() {
       {/* Background gradient image */}
       <div 
         className="absolute inset-0 w-full h-full bg-cover bg-center bg-no-repeat pointer-events-none"
-        style={{ backgroundImage: `url(${heroGradient})` }}
+        style={{ 
+          backgroundImage: `url(${heroGradient})`,
+          backgroundPosition: "20% center" 
+        }}
       />
       
       {/* Overlay for text readability */}
